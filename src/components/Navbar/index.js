9c429ba6f0d41c16e21f18f408b1c6e5c20e08d9
Alexis Padilla <alexiz.padilla11@gmail.com>
2020-12-04 22:47:42 -0600

    Init project

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { withRouter, Link } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
+
+const NavbarPage = () => (
+    <Navbar expand="md">
+        <Navbar.Brand>
+            <Link to="/">
+                <img
+                    src="assets/img/logo.png"
+                    width="100"
+                    height="65"
+                    className="d-inline-block align-top"
+                    alt="Hugo"
+                    className="mb-1"
+                />
+            </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+                <Link to="counter" className="nav-link">Counter</Link>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+);
+
+export default withRouter(NavbarPage);
