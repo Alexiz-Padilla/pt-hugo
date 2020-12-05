d21da578e95f5dcd8ac024548c54e6db03033e02
Alexis Padilla <alexiz.padilla11@gmail.com>
2020-12-05 09:15:49 -0600

    Excersite Flight

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 import CounterReducer from './CounterReducer';
 import TemperatureReducer from './TemperatureReducer';
+import FlightReducer from './FlightReducer';
 
 const createRootReducer = history => combineReducers({
     CounterReducer,
     TemperatureReducer,
+    FlightReducer,
     router: connectRouter(history),
 });
 
